e0d0a1d50cfd3cc948f781479eba3398be277bcb
rafa-rrayes <rafa@rayes.com.br>
2023-05-10 20:38:14 -0300

    Update

--- Inicio.py
+++ Inicio.py
@@ -2,7 +2,6 @@ import math
 from PIL import Image
 import numpy as np
 import os
-import imageio
 def normalize(vector):
     return vector / np.linalg.norm(vector)
 
@@ -36,11 +35,10 @@ def random_hemisphere_direction(normal):
             if np.dot(random_direction, normal) > 0:
                 return random_direction
 class Sphere:
-    def __init__(self, center, radius, color, luminous=False, luminosity=0):
+    def __init__(self, center, radius, color, luminous=False):
         self.center = np.array(center)
         self.radius = radius
         self.luminous = luminous
-        self.luminosity = luminosity
         self.color = np.array([color])/255
 class Ray:
     def __init__(self, origin, direction):
@@ -49,79 +47,50 @@ class Ray:
         self.color = np.array([255,255,255])/255
         self.lit = False
         self.pos = []
-        self.exist = True
-        self.luminosity = 0
         self.sphereHit = None
         self.bounces = 0
-    def trace(self, spheres, max_bounces):
-        hit = False
-        list_hits =[]
-        for sphere in spheres[::-1]:
+    def trace(self, spheres, max_bounces, reflectedRays = 2):
+        first_hit = None
+        global tempoForSpheres
+        global tempoForRays
+        min_distance = float('inf')
+        min_distance = float('inf')
+        first_hit = None
+        for sphere in spheres:
             hit_point, new_direction, new_color = self.reflect_ray(sphere)
             if hit_point is not None:
                 distance = np.linalg.norm(hit_point - self.origin)
-                list_hits.append([distance, hit_point, new_direction, new_color, sphere])
-                hit = True
-        if hit:
-            first_Hit = None
-            min_distance = float('inf')
-            for hit in list_hits:
-                if hit[0] < min_distance:
-                    min_distance = hit[0]
-                    first_Hit = hit
-            if first_Hit[4].luminous and self.bounces == 0:
-                return first_Hit[4].color
-            self.sphereHit = first_Hit[4]
-            self.direction = first_Hit[2]
-            self.origin = first_Hit[1]
-            self.color = first_Hit[3]
-            if self.lit == False:
-                if first_Hit[4].luminous:
-                    self.luminosity = first_Hit[4].luminosity
-                    self.lit = True
-        
+                if distance < min_distance:
+                    min_distance = distance
+                    first_hit = [hit_point, new_direction, new_color, sphere]
+        if first_hit is not None:
+            if first_hit[3].luminous and self.bounces == 0 and False: # True para fazer as luzes não refletirem
+                return first_hit[3].color
+            self.sphereHit, self.direction, self.origin, self.color = first_hit[3], first_hit[1], first_hit[0], first_hit[2]
+            self.lit = self.lit or first_hit[3].luminous
             if max_bounces > 0:
-                colors = []
-                weights = []
                 self.bounces += 1
-
-                for i in range(50):
-                    normal = normalize(self.origin - self.sphereHit.center)
-                    direction = random_hemisphere_direction(normal)
-                    new_ray = Ray(self.origin, direction)
-                    new_ray.color = self.color
-                    new_ray.lit = self.lit
-                    new_ray.bounces = self.bounces
+                colors = np.zeros((reflectedRays, 3))
+                for i in range(reflectedRays):
+                    new_ray = Ray(self.origin, random_hemisphere_direction(normalize(self.origin - self.sphereHit.center)))
+                    new_ray.color, new_ray.lit, new_ray.bounces = self.color, self.lit, self.bounces
                     color2 = new_ray.trace(spheres, max_bounces - 1)
-
                     self.lit = new_ray.lit
-                    if new_ray.lit:
-                        colors.append(color2)
-                    else:
-                        colors.append(np.array([0,0,0]))
-                self.color = np.vstack(colors).mean(axis=0)
-
-                
-        else:
-            self.exist = False
+                    colors[i] = color2 if new_ray.lit else np.array([0,0,0])
+                self.color = colors.mean(axis=0)
         if self.lit:
             return self.color
         return np.array([0,0,0])
 
     def reflect_ray(self, sphere):
+        global tempoReflect
         t = ray_sphere_intersection(self.origin, self.direction, sphere.center, sphere.radius)
-
         if t is None:
             return None, None, None
         hit_point = self.origin + t * self.direction
-        normal = normalize(hit_point - sphere.center)
-
         # Random direction in the hemisphere defined by the normal
-        reflected_direction = random_hemisphere_direction(normal)
-
-        new_origin = hit_point + 0.001 * reflected_direction
-        new_color = self.color * sphere.color
-        return new_origin, reflected_direction, new_color
+        reflected_direction = random_hemisphere_direction(normalize(hit_point - sphere.center))
+        return hit_point + 0.001 * reflected_direction, reflected_direction, self.color * sphere.color
 class Camera:
     def __init__(self, position, rotation, width, height, fov):
         self.position = position
@@ -131,8 +100,6 @@ class Camera:
         self.fov =math.radians(fov)
     def apply_rotation(self, direction):
         rx, ry, rz = self.rotation
-
-        # Rotation matrices
         rot_x = np.array([[1, 0, 0],
                              [0, math.cos(rx), -math.sin(rx)],
                              [0, math.sin(rx), math.cos(rx)]])
@@ -144,8 +111,6 @@ class Camera:
         rot_z = np.array([[math.cos(rz), -math.sin(rz), 0],
                              [math.sin(rz), math.cos(rz), 0],
                              [0, 0, 1]])
-
-        # Apply rotations
         direction = direction.dot(rot_x).dot(rot_y).dot(rot_z)
 
         return direction
@@ -156,6 +121,7 @@ class Camera:
         aspect_ratio = self.width / self.height
         scale = math.tan(self.fov / 2)
         for x in range(self.width):
+            os.system('clear')
             print(x/self.width*100, "%")
             for y in range(self.height):
                 ndc_x = (x + 0.5) / self.width
@@ -166,16 +132,16 @@ class Camera:
 
                 ray_direction = normalize(np.array([screen_x, screen_y, -1]))
                 ray_direction = self.apply_rotation(ray_direction)
-                ray = Ray(self.position, ray_direction)
-                ray.pos = [x,y]
-                color = ray.trace(spheres, 2)
+                offset = np.random.uniform(-0.0007, 0.0007, size=ray_direction.shape)
+                ray = Ray(self.position, ray_direction + offset)
+                color = ray.trace(spheres, max_bounces, reflectedRays)
                 pixels[x, y] = tuple(map(int, (color.flatten() * 255).astype(int)))
         return image
 
-bola = Sphere([0, -0, -2000], 2000, [255, 230, 0], True, 1)
-bola2 = Sphere([0, 0, 50], 50, [10, 50, 250])
-bola3 = Sphere([0,150, 0], 100, [34, 135, 156])
+bola = Sphere([0, -0, -2000], 2000, [255, 230, 42], True)
+bola2 = Sphere([0, 200, 50], 50, [230, 50, 250])
+bola3 = Sphere([0,0, 100], 100, [234, 135, 156])
 camera = Camera([400, 0, 200], [-1.5,0,-1.5], 1000, 700, 60)
+image = camera.render([bola2, bola, bola3], 2, 10)
 
-image = camera.render([bola2, bola, bola3], 1)
 image.show()
